72de3bb9d651e07aad0e93b850fac1df1c2c5a02
darkjavasript <anishsinha680@gmail.com>
2023-03-08 19:52:17 +0530

    examples of props in class component

---
+++ prop1/src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Nav from "./Nav";
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Nav data=" Wish You Happy Holi to all of you"/>
+      </div>
+    );
+  }
+}
+
+export default App;
